e44516ef4fb802d7ba20880dab3d15649b6c688a
yann1121 <yann1121@users.noreply.github.com>
2022-03-16 12:05:58 -0600

    Add files via upload

---
+++ funcion pares.js
@@ -0,0 +1,18 @@
+//funcion pares
+let array = [5,5,6,2,9,1,2,8,3,6];
+let pares = [];
+let j=1;
+ for(let i= 0;i<array.length;i++){
+    if(i % 2 ==0){
+        impares.push(array[i]);
+    }
+    else{
+        pares.push(array[i]);
+    }
+    
+      for (j; j < array.length; j++) {
+  
+}
+}
+
+console.log("Los numeros pares son " + pares);
